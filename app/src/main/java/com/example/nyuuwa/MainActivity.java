a4dc6bc147087836d079a9152a2fac56e004dc13
SANDAY-JS <san.sh.s@i.softbank.jp>
2022-09-16 12:56:37 +0900

    改行を追加

--- app/src/main/java/com/example/nyuuwa/MainActivity.java
+++ app/src/main/java/com/example/nyuuwa/MainActivity.java
@@ -106,8 +106,12 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             // 入力テキストがない場合
             return;
         }
-        // アイコンと名前を追加
-        text = "\uD83E\uDDD4　" + text;
+        if(text.length() < 10) {
+            // アイコンと名前を追加
+            text = "\uD83E\uDDD4　" + text;
+        } else {
+            text = "\uD83E\uDDD4　\n " + text;
+        }
         adapter.add(text);
 
 //        文末に「。」追加
